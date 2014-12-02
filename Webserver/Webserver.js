ee9f36a8564e1794de379ec185dfc63dddd2510a
Adam Mahon <amahon.1123@gmail.com>
2014-12-01 22:49:36 -0500

    Major Overhaul of Webserver
    Webserver now gets XML from the source, and rehosts. This prevents error
    from website being down/Timeout issues.

--- Webserver/Webserver.js
+++ Webserver/Webserver.js
@@ -4,9 +4,29 @@ var path = require('path');
 var port = 8080;
 app.listen(port);
 var echo = [];
+var request = require('request');
+var fs = require('fs');
+var newsSource;
+var destination;
 
 //Outputs current port
 console.log('Port: ' + port);
+
+
+/**********************************************
+Receiving Data
+**********************************************/
+newsSource = ["http://www.iwusojourn.com/feed/", "http://www.iwupresident.com/feed/", "http://www.iwusga.com/feed/", "http://www.iwuspectrum.com/feed/", "http://www.iwuwildcats.com/rss.php"];
+destination = ["XML/sojourn.xml", "XML/president.xml", "XML/sga.xml", "XML/spectrum.xml", "XML/athletics.xml"]
+//console.log(newsSource.length);
+
+for(var i=0; i<newsSource.length; i++){
+	setTimeout(newsRequest(newsSource[i],destination[i]),20000);
+}
+
+/**********************************************
+Sending Data
+**********************************************/
 
 // Baldwin Menu
 app.get('/baldwin', function(req, res){
@@ -26,32 +46,36 @@ app.get('/chapel', function(req, res){
 app.get('/athletics', function(req, res){
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	res.sendFile(path.join(__dirname, 'athletics.xml'));
+	res.sendFile(path.join(__dirname, 'XML/athletics.xml'));
 });
+
+/*
 app.get('/portal', function(req, res){
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	res.sendFile(path.join(__dirname, 'portal.xml'));
 });
+*/
+
 app.get('/president', function(req, res){
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	res.sendFile(path.join(__dirname, 'president.xml'));
+	res.sendFile(path.join(__dirname, 'XML/president.xml'));
 });
 app.get('/sga', function(req, res){
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	res.sendFile(path.join(__dirname, 'sga.xml'));
+	res.sendFile(path.join(__dirname, 'XML/sga.xml'));
 });
 app.get('/sojourn', function(req, res){
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	res.sendFile(path.join(__dirname, 'sojourn.xml'));
+	res.sendFile(path.join(__dirname, 'XML/sojourn.xml'));
 });
 app.get('/spectrum', function(req, res){
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	res.sendFile(path.join(__dirname, 'spectrum.xml'));
+	res.sendFile(path.join(__dirname, 'XML/spectrum.xml'));
 });
 
 // Verse of the Day
@@ -72,3 +96,22 @@ app.get('/weather-future', function(req, res){
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	res.sendFile(path.join(__dirname, 'weather-future.xml'));
 });
+
+/*******************************************
+Function Calls
+*******************************************/
+
+function newsRequest(url, file) {
+	//console.log(url);
+	request(url, function (error, response, body){
+		if (response.statusCode == 200){
+			//console.log("success");
+			//console.log(response.statusCode);
+			request(url).pipe(fs.createWriteStream(file));
+		}
+		else {
+			//console.log("I done messed up");
+			//console.log(response.statusCode);
+		}
+	})
+}
