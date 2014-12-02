dd6c97695a1f01a60d28d5665ceef8263b8542e6
Adam Mahon <amahon.1123@gmail.com>
2014-12-01 21:14:59 -0500

    Updated webserver to host votd and weather
    Add votd(Verse of the Day) and weather xml to the webserver.

--- Webserver/Webserver.js
+++ Webserver/Webserver.js
@@ -13,51 +13,62 @@ app.get('/baldwin', function(req, res){
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	res.sendFile(path.join(__dirname, 'baldwin.xml'));
-})
+});
 
 // Chapel Schedule
 app.get('/chapel', function(req, res){
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	res.sendFile(path.join(__dirname, 'chapel.xml'));
-})
+});
 
 // News Sources
 app.get('/athletics', function(req, res){
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	res.sendFile(path.join(__dirname, 'athletics.xml'));
-})
+});
 app.get('/portal', function(req, res){
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	res.sendFile(path.join(__dirname, 'portal.xml'));
-})
+});
 app.get('/president', function(req, res){
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	res.sendFile(path.join(__dirname, 'president.xml'));
-})
+});
 app.get('/sga', function(req, res){
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	res.sendFile(path.join(__dirname, 'sga.xml'));
-})
+});
 app.get('/sojourn', function(req, res){
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	res.sendFile(path.join(__dirname, 'sojourn.xml'));
-})
+});
 app.get('/spectrum', function(req, res){
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	res.sendFile(path.join(__dirname, 'spectrum.xml'));
-})
+});
 
 // Verse of the Day
 app.get('/votd', function(req, res){
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	res.sendFile(path.join(__dirname, 'votd.xml'));
-})
+});
 
+// Current / Future Weather
+app.get('/weather-current', function(req, res){
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.sendFile(path.join(__dirname, 'weather-current.xml'));
+});
+app.get('/weather-future', function(req, res){
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.sendFile(path.join(__dirname, 'weather-future.xml'));
+});
