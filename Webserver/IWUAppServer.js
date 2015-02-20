fc8aa84a386c7c9b6e60bf0d8d1eca348ef29bd0
Harrison Oliver <tyler.oliver18@gmail.com>
2015-02-20 11:10:31 -0500

    Updated Server -- Broken, still changing
    Making some major changes to the way the server works. Going to begin
    implementing promises, have working example, but server is currently
    down

--- Webserver/IWUAppServer.js
+++ Webserver/IWUAppServer.js
@@ -12,6 +12,7 @@ var $ = require('jquery');                //Allows use of jquery
 var jsdom = require("jsdom");             //Allows use of jsdom
 var window = jsdom.jsdom().parentWindow;  //creates windows for jquery
 var port = 8080;                          //port for server
+var Promise = require('promise');
 app.listen(port);                         //event listener on port
 console.log('Port: ' + port);             //Outputs current port
 
@@ -50,7 +51,7 @@ app.get('/baldwin', function(req, res){
 app.get('/chapel', function(req, res){
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.sendFile(path.join(__dirname, 'chapel.xml'));
+  res.sendFile(path.join(__dirname, 'XML/chapel.json'));
 });
 
 // News
@@ -65,7 +66,8 @@ app.get('/news', function(req, res){
 app.get('/votd', function(req, res){
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.sendFile(path.join(__dirname, 'votd.xml'));
+  res.sendFile(path.join(__dirname, 'XML/votd.json'));
+
 });
 
 // Current / Future Weather
@@ -145,9 +147,6 @@ function parseXML(tempString, callback){
 
         //clean up XML by removing tab, new line, and other junk characters
         channelTitle = $(this).children('title').text();
-        channelTitle = channelTitle.replace(/(\\t0|\\n|\\t|]]>|\uFFFD)/g,"");
-        channelTitle = channelTitle.replace(/\&/g,"&amp;");
-        
 
         $(this).children('item').each(function(){ //In each item inside each channel inside each RSS tag
 
@@ -155,8 +154,6 @@ function parseXML(tempString, callback){
 
           //clean up XML by removing tab, new line, and other junk characters
           itemTitle = $(this).children('title').text();
-          itemTitle = itemTitle.replace(/(\\t0|\\n|\\t|]]>|\uFFFD)/g,"");
-          itemTitle = itemTitle.replace(/\&/g,"&amp;");
           if(itemTitle==""){
             itemTitle = "False";
           }
@@ -176,8 +173,6 @@ function parseXML(tempString, callback){
 
           //clean up XML by removing tab, new line, and other junk characters
           description = $(this).children('description').text();
-          description = description.replace(/(\\t0|\\n|\\t|]]>|\uFFFD)/g,"");
-          description = description.replace(/\&/g,"&amp;");
           if(description==""){
             description = "False";
           }
@@ -191,13 +186,11 @@ function parseXML(tempString, callback){
 
           //clean up XML by removing tab, new line, and other junk characters
           content = $(this).children('encoded').text();
-          content = content.replace(/(\\t0|\\n|\\t|]]>|\uFFFD)/g,"");
-          content = content.replace(/\&/g,"&amp;");
           if(content==""){
             content = "False";
           }
           newsItemsArray[i]['content'] = content; //Add content to array
-		      
+          
           newsArrayLength++; //Increment a variable representing length of array
           i++;
         });        
@@ -226,21 +219,16 @@ function fillXML(callback){
 //Places the array of items into an rss feed, then puts the feed into news.xml
 
   console.log('JSON initialized') 
-  /*
-  newsString = "<root>";
-
-  for (var l=0; l<newsArrayLength; l++){
-    newsString += "\n\t<item>\n\t\t<title>" + newsItemsArray[l]['item'] + "</title>\n\t\t<channel>" + newsItemsArray[l]['channel'] + "</channel>\n\t\t<guid>" + newsItemsArray[l]['guid'] + "</guid>\n\t\t<description>" + newsItemsArray[l]['description'] + "</description>\n\t\t<date>"
-     + newsItemsArray[l]['date'] + "</date>\n\t\t<content>" + newsItemsArray[l]['content'] + "</content>\n\t</item>";
-    //NewsString contains all the array contents. We're preparing to add these array contents to our XML
-  }
-
-  newsString += "\n</root>"; //Add closing tag
-  */
   newsString = '{"news":'
   newsString += JSON.stringify(newsItemsArray);
   newsString += "}"
 
+  newsString = newsString.replace(/(\\t0|\\n|\\t|]]>|\uFFFD)/g,"");
+  newsString = newsString.replace(/â€™/g, "");
+  //newsString = newsString.replace(/\&/g,"&amp;");
+
+  
+  
   fs.writeFileSync('XML/news.JSON',newsString); //Write to file
   console.log("File Written");
 
@@ -257,6 +245,9 @@ function sortNews(){
   for(var i=0; i<newsSource.length-1; i++){
       newsRequest(newsSource[i],destination[i]);
   }
+
+  newsRequest("https://www.biblegateway.com/votd/get/?format=json","XML/tempvotd.json");
+
 
 //Runs functions one after another, beginning with UpdateLocalData and ending in closeXML. Currently does not run properly. 
   updateLocalData(function(tempString){
